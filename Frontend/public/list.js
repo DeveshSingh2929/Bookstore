1163a387322476e60d1c2922a070953a681583ff
pinki singh <Pinkisingh1820@gmail.com>
2024-08-01 23:17:01 +0530

    initial commit

---
+++ Frontend/public/list.js
@@ -0,0 +1,63 @@
+// Define the list of storybooks
+const list = [
+    {
+        "id": 1,
+        "name": "Fairy Tail Stories",
+        "title": "Fairy Tail",
+        "price": 25,
+        "category": "Free",
+        "image": "https://img.freepik.com/free-photo/open-book-concept-fiction-storytelling-fairytale_23-2150793759.jpg?t=st=1713698164~exp=1713701764~hmac=7593e69ef995108825417636d4112fb2dd6737020735c346476239f7715ace0b&w=996"
+    },
+    {
+        "id": 2,
+        "name": "Harry PotterHarry Potter and the Sorcerer's Stone",
+        "title": "Fiction",
+        "price": 60,
+        "category": "Free",
+        "image": "https://img.freepik.com/premium-photo/illustration-wizard_931878-419308.jpg?w=740"
+    },
+    {
+        "id": 3,
+        "name": "Harry Potter and the Chamber of Secrets",
+        "title": "Fiction",
+        "price": 50,
+        "category": "Free",
+        "image": "https://img.freepik.com/free-photo/education-day-scene-fantasy-style-aesthetic_23-2151040234.jpg?t=st=1713698100~exp=1713701700~hmac=e353a59b263972a3372a26f7f9186222aa65f779890b0ad0850859a3ad411247&w=740"
+    },
+    {
+        "id": 4,
+        "name": "Harry Potter and the Prisoner of Azkaban",
+        "title": "Fiction",
+        "price": 50,
+        "category": "Free",
+        "image": "https://img.freepik.com/free-vector/halloween-haunted-house-with-flat-design_23-2147923623.jpg?t=st=1713698217~exp=1713701817~hmac=b7d7d802bbd5a5992952945538cf22cccb70e0fb05fc566a62af69dd118a874f&w=740"
+    },
+    {
+        "id": 5,
+        "name": "Harry Potter and the Order of the Phoenix",
+        "title": "Fiction",
+        "price": 50,
+        "category": "Free",
+        "image": "https://img.freepik.com/free-vector/halloween-background-with-spooky-haunted-house_1048-3076.jpg?t=st=1713698310~exp=1713701910~hmac=080dfe8f0ba92c4656cab5dfbbc01eb90746286bc91ae6ecfca128adf0c0a4cc&w=740"
+    },
+    {
+        "id": 6,
+        "name": "Sherlock Holmes - The Complete Collection",
+        "title": "Fiction",
+        "price": 150,
+        "category": "Free",
+        "image": "https://img.freepik.com/free-vector/mysterious-gangster-character_23-2148473800.jpg?t=st=1713698016~exp=1713701616~hmac=0199e7740bae0f7fee1cb1db44e0f0f8299d910ee6112286d03b2db9b466577f&w=740"
+    },
+    {
+        "id": 7,
+        "name": "The History of Quantum Physics",
+        "title": "Science",
+        "price": 30,
+        "category": "Free",
+        "image": "https://img.freepik.com/free-vector/gradient-quantum-illustration_52683-80504.jpg?t=st=1713697983~exp=1713701583~hmac=e8faffe65bc8924995b9ba53c5f317ea25fd9319ad732b23b6d613246b7a1fb3&w=740"
+    },
+    // Add more book objects here...
+];
+
+// Export the list
+export default list;
